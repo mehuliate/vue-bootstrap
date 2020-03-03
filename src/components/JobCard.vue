67a519ed26520963763002edf78d6077669cbcb3
mehuliate <darksideofbotix@gmail.com>
2020-03-03 23:36:03 +0700

    first commit

---
+++ src/components/JobCard.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <b-col>
+			<b-card
+				:title="name"
+				img-src="https://picsum.photos/600/300/?image=25"
+				img-alt="job-info"
+				class="mb-3"
+				img-top
+				tag="article"
+				style="max-width: 20rem"
+                footer="Card Footer"
+			>
+				<b-card-text>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis repudiandae possi</b-card-text>
+				<b-button
+					variant="primary"
+				>Apply</b-button>
+			</b-card>
+		</b-col>
+
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['name']
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,56 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-container>
+		<b-row align-v="center">
+			<JobCard v-for="job in displayJob" :key="job.id" :name="job.name"></JobCard>
+		</b-row>
+		<b-pagination
+		v-model="currentPage"
+		:total-rows="rows"
+		:per-page="perPage"
+		first-text="First"
+		prev-text="Prev"
+		next-text="Next"
+		last-text="Last"
+		@input="paginate(currentPage)"
+		></b-pagination>
+      
+    </b-container>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import JobCard from "@/components/JobCard.vue";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+	name: "home",
+	components: {"JobCard": JobCard },
+	mounted(){
+		this.fetchData();
+	},
+	data(){
+		return {
+			jobs : [],
+			displayJob: [],
+			currentPage: 1,
+			rows:1,
+			perPage: 3
+		}
+	},
+	methods: {
+		async fetchData(){
+			const res = await fetch("data.json");
+			const val = await res.json();
+			this.jobs = val;
+			this.displayJob = val.slice(0,3);
+			this.rows = this.jobs.length;
+		},
+		paginate(currentPage){
+			console.log(currentPage);
+			const start = (currentPage - 1 ) * this.perPage;
+			this.displayJob = this.jobs.slice(start, start+3);
+		}
+	}
 }
 </script>
